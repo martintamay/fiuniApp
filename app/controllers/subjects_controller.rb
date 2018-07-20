cc5f3a630d62b761ab548acd1965b5e2edc69139
Martín Tamay <martintamay9@gmail.com>
2018-07-19 20:38:00 -0400

    se implementa el get de notes en subjects

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -35,6 +35,15 @@ class SubjectsController < ApplicationController
     end
   end
 
+  def notes
+    subject = Subject.find_by_id(params[:subject_id])
+    if(subject)
+      render json: subject.notes
+    else
+      render json: {}, status: :not_found
+    end
+  end
+
   private
     def subject_params
       params.require(:subject).permit(:name, :semester, :professor_id)
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,11 +1,22 @@
 class Subject < ApplicationRecord
   belongs_to :professor, optional: true
+  has_many :takens
 
   def as_json(options={})
     if(options[:only]==nil)
       super(:only => [:id,:name,:semester,:profesor])
     else
       super(options)
+    end
+  end
+
+  def notes
+    self.takens.map do |taken|
+      taken.student.as_json(:only => [:id,:entry_year],
+        include: {
+          person: { :only => [:names,:ci] }
+        }
+      ).merge({ "notes" => taken.notes.as_json})
     end
   end
 end
